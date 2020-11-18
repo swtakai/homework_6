38c21a077030c44ce480c38c2822e489b1268afd
Saya <swtakai@umich.edu>
2020-11-18 04:17:21 -0500

    fixed probably

--- js/video.js
+++ js/video.js
@@ -9,6 +9,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+	let vol = document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
